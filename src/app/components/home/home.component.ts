6b4106f7b12bdf86d5bcbb5d380ed1ac38ec2931
AlecDusheck <a.dusheckirl@gmail.com>
2020-04-03 22:00:45 -0500

    Re-do home page

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,11 +1,18 @@
 import {Component, OnInit} from '@angular/core';
 
+import { faLinkedin, faTwitter } from '@fortawesome/free-brands-svg-icons';
+
+
 @Component({
     selector: 'app-home',
     templateUrl: './home.component.html',
     styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
+
+    // Import FontAwesome icons
+    linkedIn = faLinkedin;
+    twitter = faTwitter;
 
     constructor() {
     }
